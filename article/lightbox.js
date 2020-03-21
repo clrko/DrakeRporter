6f5e1750684cb7fb997215b92ef72507f7b5dafb
Claire Kodia <claire.kodia@gmail.com>
2020-03-21 21:20:27 +0100

    Code for the lightbox done up to the 35 min of the tuto

--- article/lightbox.js
+++ article/lightbox.js
@@ -1,8 +1,12 @@
 /*ck 1 step regarder vers 22minutes*/
+
+/**
+ * @property {HTMLElement} element
+ */
 class lightbox {
     static init() { /*methode statique qui permet d'initialise rune lightbox Objectif est greffer un comportement sur tous les liens qui mene a une image. */
         const links = document.querySelectorAll('a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"]');
-        .forEach(link => link.addEventListener('click', e => {
+        forEach(link => link.addEventListener('click', e => {
             e.preventDefault()
             new lightbox(e.currentTarget.getAttribute('href'));
         }))
@@ -13,10 +17,44 @@ class lightbox {
      * @param {string} url URL de l'image 
      */
     constructor (url) {
-        const element = this.buildDOM(url)
-        document.body.appendChild(element)
+        this.element = this.buildDOM(url)
+        this.loadImage(url)
+        document.body.appendChild(this.element)
+    }
+
+    /**
+     * 
+     * @param {string} url URL de l'image
+     */
+    loadImage (url) {
+        const image = new Image()
+        const container = this.element.querySelector('.lightbox__container')
+        const loader = document.createElement('div')
+        loader.classList.add('lightbox__loader')
+        container.appendChild(loader)
+        image.onload = function () {
+            container.removeChild(loader)
+            container.appendChild(image)
+        };
+        image.src = url;
     }
 /*ck 2nd step : construire la div vec la class lightbox pour une url donnée*/
+
+
+
+
+    /**
+     * Ferme la lightbox
+     * @param {MouseEvent} e 
+     */
+    close (e) {
+        e.preventDefault();
+        this.element.classList.add('faceOut')
+        window.setTimeout(() => {
+            this.element.parentElement.removeChild(this.element)
+        },500)
+    }
+
     /**
      * 
      * @param {string} url URL de l'image
@@ -30,11 +68,10 @@ class lightbox {
         <button class="lightbox__close"><i class="fas fa-times"></i></button>
         <button class="lightbox__next"><i class="fas fa-chevron-right"></i></button>
         <button class="lightbox__prev"><i class="fas fa-chevron-left"></i></button>
-        <div class="lightbox__container">
-            <img src="https://picsum.photos/900/1800" alt="">
-        </div>`
+        <div class="lightbox__container"></div>`
+        dom.querySelector('lightbox__close').addEventListener('click', this.close.bind(this))
         return dom
     }
 }
 
-lightbox ();
+lightbox;
